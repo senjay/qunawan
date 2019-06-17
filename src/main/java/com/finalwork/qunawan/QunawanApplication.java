66ec2441db6c9edf5dfa0a0006ee7d848929d540
senjay <760832791@qq.com>
2019-06-17 09:42:39 +0800

    Initial commit

---
+++ src/main/java/com/finalwork/qunawan/QunawanApplication.java
@@ -0,0 +1,13 @@
+package com.finalwork.qunawan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QunawanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QunawanApplication.class, args);
+    }
+
+}
